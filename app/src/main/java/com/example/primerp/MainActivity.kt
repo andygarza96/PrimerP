5b67877c173105540f7331a3ba22fe74d18135ea
Andrea Berrones <31523761+andygarza96@users.noreply.github.com>
2019-09-09 21:15:24 -0600

    Terminado

--- app/src/main/java/com/example/primerp/MainActivity.kt
+++ app/src/main/java/com/example/primerp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -50,10 +51,10 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "El Actual de manzanas: ${mactual.toString()}", Toast.LENGTH_LONG).show()
         }
         btnCalcular.setOnClickListener {
-            var Porcentaje: Double = (pactual * 100)/(etxtTotal.text.toString().toDouble()* man)
+            var Porcentaje: Double = (pactual * 100)/(etxtTotal.text.toString().toDouble())
             Toast.makeText(this, "El Porcentaje de manzanas: ${Porcentaje.toString()}", Toast.LENGTH_LONG).show()
-
-
+            txtPactual.text= Porcentaje.toString()
+            if(Porcentaje > 69) { fondo() }
         }
 
     }
@@ -84,6 +85,10 @@ class MainActivity : AppCompatActivity() {
         val newScore = getString(R.string.manzanas, pactual.toString())
         txtActual.text=newScore
 
+    }
+    fun fondo() {
+        val color = ContextCompat.getColor(this, R.color.Red)
+        fondo.setBackgroundColor(color)
     }
 }
 
